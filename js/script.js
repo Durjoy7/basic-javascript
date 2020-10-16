532bfc6045a70c2d09ef5044425cb90ee23225a9
Durjoy <durjoydatta3459@gmail.com>
2020-10-16 10:57:56 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//javaScript Output
+
+
+/*
+console.Log()
+document.write()
+innerHTML
+aLert()
+
+*/
+
+//Data Type
+/*
+string='JavaScript;
+number=12
+symbol
+null
+
+*/
+
+
+//variable
+/*
+var
+let
+const
+
+*/
+
+//var x="PP"
+//var x="AB"
+//var x="JS"
+
+//let x='javascript'
+
+
+let learning="javascript is best"
+let check=learning.length
+    check=learning.concat(" ","ever")
+
+    check=learning.toLocaleUpperCase()
+
+    check=learning.substring(0,5)
+
+    check=learning.slice(-7)
+
+    check=learning.split(" ")
+console.log(check)
+ 
